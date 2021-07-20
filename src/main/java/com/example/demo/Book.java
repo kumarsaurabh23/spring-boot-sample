32fa1d0f8072e182e7230efeff60ffe107b39743
saurabh <kumarsaurabh23@gmail.com>
2021-07-19 22:15:27 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Book {
+    private int id;
+    private String name;
+}
